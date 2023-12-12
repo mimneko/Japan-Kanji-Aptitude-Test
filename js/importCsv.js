3d348d21fddc321eb164d823ac87bb0ef50a7b0e
みむねこ <153196561+mimneko@users.noreply.github.com>
2023-12-11 19:37:04 -0800

    Update importCsv.js

--- js/importCsv.js
+++ js/importCsv.js
@@ -1,56 +1,56 @@
 let arr = [];
 
 window.onload = function () {
-            //出力場所を探す
-            let output = document.getElementById("output");
-            //CSVの指定
-            getCsv('semi-1.csv');
+	//出力場所を探す
+	let output = document.getElementById("output");
+	//CSVの指定
+    getCsv('semi-1.csv');
 
-            //CSVの取り込み
-            function getCsv(data) {
-                // HTTPでファイルを読み込む
-                let xhr = new XMLHttpRequest();
-                //取得するファイルの設定
-                xhr.open("GET", data, true);
-                //レスポンスの確認
-                xhr.onload = function (e) {
-                    if (xhr.readyState === 4) {//4は完了
-                        if (xhr.status === 200) {//Done or load
-                            //console.log(xhr.responseText);
-                            let responce = xhr.responseText;
-                            csvArr(responce);
-                        } else {
-                            console.error(xhr.statusText);
-                        }
-                    }
-                };
-                //リクエストの要求送信
-                xhr.send(null);
-            }
-
-            //CSVを配列に格納 dataArrはレスポンス
-            function csvArr(dataArr) {
-                let list = dataArr.split('\n');
-                //帰ってきているレスポンスを配列に格納する
-                for (let i = 0; i < list.length; i++) {
-                    arr[i] = list[i].split(',');
+    //CSVの取り込み
+    function getCsv(data) {
+        // HTTPでファイルを読み込む
+        let xhr = new XMLHttpRequest();
+        //取得するファイルの設定
+        xhr.open("GET", data, true);
+        //レスポンスの確認
+        xhr.onload = function (e) {
+            if (xhr.readyState === 4) {//4は完了
+                if (xhr.status === 200) {//Done or load
+                    //console.log(xhr.responseText);
+                    let responce = xhr.responseText;
+                    csvArr(responce);
+                } else {
+                    console.error(xhr.statusText);
                 }
-                //console.log(arr);
-                htmlWrite(arr);//出力をtableに設定する
             }
+        };
+        //リクエストの要求送信
+        xhr.send(null);
+    }
 
-            //出力のタグを設定
-            function htmlWrite(dataList) {
-                let insert = "";
-                dataList.forEach(element => {
-                    insert += '<tr>';
-                    element.forEach((childElement) => {
-                        insert += `<td>${childElement}</td>`;
-                    });
-                    insert += '</tr>'
-                });
-
-                //HTMLに出力
-                output.innerHTML = insert;
-            }
+    //CSVを配列に格納 dataArrはレスポンス
+    function csvArr(dataArr) {
+        let list = dataArr.split('\n');
+        //帰ってきているレスポンスを配列に格納する
+        for (let i = 0; i < list.length; i++) {
+            arr[i] = list[i].split(',');
         }
+        //console.log(arr);
+        htmlWrite(arr);//出力をtableに設定する
+    }
+
+    //出力のタグを設定
+    function htmlWrite(dataList) {
+        let insert = "";
+        dataList.forEach(element => {
+            insert += '<tr>';
+            element.forEach((childElement) => {
+                insert += `<td>${childElement}</td>`;
+            });
+            insert += '</tr>'
+        });
+
+        //HTMLに出力
+        output.innerHTML = insert;
+    }
+}
