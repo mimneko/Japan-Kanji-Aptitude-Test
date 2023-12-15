b14d39e3bb1f88bc021f18c1c9a50f15c3406100
みむねこ <153196561+mimneko@users.noreply.github.com>
2023-12-14 21:11:27 -0800

    Update buttonAction.js

--- js/buttonAction.js
+++ js/buttonAction.js
@@ -9,9 +9,9 @@ $(function () {
     $("#next").click(function () {
         $("#question").text("【問題】" + arr[getRandomIntInclusive(2, 5396)][6]);
     })
-//});
+});
 
-//$(function () {
+$(function () {
     let htmlId = "#a1-1"
     $(htmlId).click(function () {
         $(htmlId).html('<span class="answer">けつがん</span>');
