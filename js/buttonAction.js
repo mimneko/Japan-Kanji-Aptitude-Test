1309acc8944d702f8ea71204452d19c4b4bfffb4
みむねこ <153196561+mimneko@users.noreply.github.com>
2023-12-17 15:32:03 +0900

    Update buttonAction.js

--- js/buttonAction.js
+++ js/buttonAction.js
@@ -7,7 +7,7 @@ function getRandomIntInclusive(min, max) {
 
 $(function () {
     $("#next").click(function () {
-        $("#question").text("【問題】" + arr[getRandomIntInclusive(2, 5396)][6]);
+        $("#question").text("【問題】" + arr[getRandomIntInclusive(2, 5)][0]);
     })
 });
 
